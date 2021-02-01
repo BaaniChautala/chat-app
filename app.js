8c4189de66244b27138f10ef01a3e9240eac5427
VivekMethew <46779046+VivekMethew@users.noreply.github.com>
2021-02-01 11:37:33 +0530

    chat app

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 3000
+const server = app.listen(PORT, () => { console.log('server on port', PORT) })
+
+const io = require('socket.io')(server)
+app.use(express.static(path.join(__dirname, 'public')))
+
+let socketsConnected = new Set()
+
+io.on('connection', onConneted)
+
+function onConneted(socket) {
+    // console.log(socket.id)
+    socketsConnected.add(socket.id)
+
+    io.emit('clients-total', socketsConnected.size)
+
+    socket.on('disconnect', () => {
+        // console.log('Socket disconnected', socket.id)
+        socketsConnected.delete(socket.id)
+        io.emit('clients-total', socketsConnected.size)
+    })
+
+    socket.on('message', (data) => {
+        socket.broadcast.emit('chat-message', data)
+            // console.log(data)
+    })
+
+    socket.on('feedback', (data) => {
+        socket.broadcast.emit('feedback', data)
+    })
+}
